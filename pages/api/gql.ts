d64f6e15c6544d822d9626819d2f1f25a73aba67
franco perdomo <franco.perdomo@autoflow.com.ar>
2023-03-14 15:08:22 -0300

    feat: added authentication

--- pages/api/gql.ts
+++ pages/api/gql.ts
@@ -1,21 +1,9 @@
 import { NextApiHandler } from "next";
-import { isUserAuthorized } from "@tinacms/auth";
 import { databaseRequest } from "../../lib/databaseConnection";
 
 const nextApiHandler: NextApiHandler = async (req, res) => {
   // Use your own authentication logic here
-  // const isAuthorized = headers.authorization === "Bearer some-token"
-
-  // Example if using TinaCloud for auth
-  const tinaCloudUser = await isUserAuthorized({
-    clientID: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-    token: req.headers.authorization,
-  });
-
-  const isAuthorized =
-    process.env.TINA_PUBLIC_IS_LOCAL === "true" ||
-    tinaCloudUser?.verified ||
-    false;
+  const isAuthorized = req?.headers.authorization === "Bearer some-token";
 
   if (isAuthorized) {
     const { query, variables } = req.body;
